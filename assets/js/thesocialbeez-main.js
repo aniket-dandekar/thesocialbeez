bd7101f0f1c95b1fb7e42b7c879f1cd747303f93
Aniket <57931002+aniket-dandekar@users.noreply.github.com>
2023-01-25 09:07:20 +0530

    whatsapp + contact form layout

--- assets/js/thesocialbeez-main.js
+++ assets/js/thesocialbeez-main.js
@@ -1,9 +1,10 @@
+const toggle_btn = document.querySelector('#toggle-btn');
 toggle_btn.addEventListener("click", () => {
     if (toggle_btn.getAttribute('aria-expanded') === "false") {
         toggle_btn.setAttribute('aria-expanded', 'true');
         // setTimeout(() => {
+            // }, 300);
         header__nav.setAttribute('aria-expanded', 'true');
-        // }, 300);
         document.querySelector(".header__ul").classList.remove("list_animate_close")
         document.querySelector(".header__ul").classList.add("list_animate_open")
 
@@ -14,6 +15,10 @@ toggle_btn.addEventListener("click", () => {
         }, 300);
         document.querySelector(".header__ul").classList.remove("list_animate_open")
         document.querySelector(".header__ul").classList.add("list_animate_close")
-
     }
+})
+
+const contact_form = document.querySelector("#contact-form")
+contact_form.addEventListener("submit", (e)=>{
+    e.preventDefault();
 })
