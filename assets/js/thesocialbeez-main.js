b200305b6484c571c5d8a619434657c4b53440e1
Aniket <57931002+aniket-dandekar@users.noreply.github.com>
2023-02-03 14:22:47 +0530

    blogs + work-p +

--- assets/js/thesocialbeez-main.js
+++ assets/js/thesocialbeez-main.js
@@ -1,23 +1,3 @@
-const toggle_btn = document.querySelector('#toggle-btn');
-toggle_btn.addEventListener("click", () => {
-    if (toggle_btn.getAttribute('aria-expanded') === "false") {
-        toggle_btn.setAttribute('aria-expanded', 'true');
-        // setTimeout(() => {
-            // }, 300);
-        header__nav.setAttribute('aria-expanded', 'true');
-        document.querySelector(".header__ul").classList.remove("list_animate_close")
-        document.querySelector(".header__ul").classList.add("list_animate_open")
-
-    } else {
-        toggle_btn.setAttribute('aria-expanded', 'false');
-        setTimeout(() => {
-            header__nav.setAttribute('aria-expanded', 'false');
-        }, 300);
-        document.querySelector(".header__ul").classList.remove("list_animate_open")
-        document.querySelector(".header__ul").classList.add("list_animate_close")
-    }
-})
-
 const contact_form = document.querySelector("#contact-form")
 contact_form.addEventListener("submit", (e)=>{
     e.preventDefault();
---
+++ assets/js/thesocialbeez-navbartoggle.js
@@ -0,0 +1,19 @@
+const toggle_btn = document.querySelector('#toggle-btn');
+toggle_btn.addEventListener("click", () => {
+    if (toggle_btn.getAttribute('aria-expanded') === "false") {
+        toggle_btn.setAttribute('aria-expanded', 'true');
+        // setTimeout(() => {
+            // }, 300);
+        header__nav.setAttribute('aria-expanded', 'true');
+        document.querySelector(".header__ul").classList.remove("list_animate_close")
+        document.querySelector(".header__ul").classList.add("list_animate_open")
+
+    } else {
+        toggle_btn.setAttribute('aria-expanded', 'false');
+        setTimeout(() => {
+            header__nav.setAttribute('aria-expanded', 'false');
+        }, 300);
+        document.querySelector(".header__ul").classList.remove("list_animate_open")
+        document.querySelector(".header__ul").classList.add("list_animate_close")
+    }
+})
